bfbf28643e34127922a42611dcfac7de7b2bb82f
LeoPFreitas <leonardo.cadu2@gmail.com>
2021-01-09 18:05:14 -0300

    refactor DDL script
    Signed-off-by: LeoPFreitas <leonardo.cadu2@gmail.com>

--- Project/leonardo/script.sql
+++ Project/leonardo/script.sql
@@ -1,8 +1,8 @@
--- DML
+CREATE USER IF NOT EXISTS projectUser IDENTIFIED BY 'fakepassword';
+
+-- DDL
 DROP DATABASE IF EXISTS project;
 CREATE DATABASE project;
-
-CREATE USER IF NOT EXISTS projectUser IDENTIFIED BY 'fakepassword';
 
 USE project;
 
@@ -19,8 +19,119 @@ CREATE TABLE IF NOT EXISTS pessoa
     estado          char(2),
     cidade          varchar(50),
     bairro          varchar(50),
-    rua             varchar(50),
+    rua             varchar(100),
     numero          integer
+);
+
+CREATE TABLE IF NOT EXISTS cliente
+(
+    id     bigint unique not null primary key,
+    pontos int check ( pontos >= 0 ),
+
+    FOREIGN KEY (id)
+        REFERENCES pessoa (id)
+);
+
+CREATE TABLE IF NOT EXISTS funcionario
+(
+    id      bigint unique not null primary key,
+    salario decimal(15, 2),
+    turno   enum ('M', 'T', 'N'),
+    cargo   varchar(50),
+
+    FOREIGN KEY (id)
+        REFERENCES pessoa (id)
+);
+
+CREATE TABLE IF NOT EXISTS categoria
+(
+    id        bigint primary key auto_increment unique not null,
+    descricao text
+);
+
+CREATE TABLE IF NOT EXISTS ingrediente
+(
+    id        bigint primary key auto_increment unique not null,
+    nome      varchar(100),
+    descricao text
+);
+
+CREATE TABLE IF NOT EXISTS bolo
+(
+    id        bigint primary key auto_increment unique not null,
+    categoria bigint,
+    nome      varchar(50),
+    descricao text,
+    tipo      varchar(30),
+    peso      integer check ( peso > 0 ),
+    sabor     varchar(40),
+
+    FOREIGN KEY (categoria)
+        REFERENCES pessoa (id)
+);
+
+CREATE TABLE IF NOT EXISTS venda
+(
+    id_venda         bigint primary key auto_increment unique not null,
+    data             date,
+    valor            decimal(13, 4),
+    metodo_pagamento varchar(50),
+    quantidade       integer,
+    id_cliente       bigint,
+    id_funcionario   bigint,
+
+    FOREIGN KEY (id_cliente)
+        REFERENCES cliente (id),
+    FOREIGN KEY (id_funcionario)
+        REFERENCES funcionario (id)
+);
+
+CREATE TABLE IF NOT EXISTS telefone
+(
+    id_pessoa bigint,
+    telefone  bigint(14),
+
+    PRIMARY KEY (id_pessoa, telefone),
+    FOREIGN KEY (id_pessoa)
+        REFERENCES pessoa (id)
+);
+
+CREATE TABLE IF NOT EXISTS dependente
+(
+    id              bigint unique not null,
+    id_funcionario  bigint,
+    nome            varchar(100),
+    data_nascimento date,
+
+    PRIMARY KEY (id, id_funcionario),
+    FOREIGN KEY (id_funcionario)
+        REFERENCES funcionario (id)
+);
+
+CREATE TABLE IF NOT EXISTS bolo_ingrediente
+(
+    id_bolo        bigint,
+    id_ingrediente bigint,
+    quantidade     integer,
+
+    PRIMARY KEY (id_bolo, id_ingrediente),
+    FOREIGN KEY (id_bolo)
+        REFERENCES bolo (id),
+    FOREIGN KEY (id_ingrediente)
+        REFERENCES ingrediente (id)
+);
+
+CREATE TABLE IF NOT EXISTS venda_bolo
+(
+    id_bolo    bigint,
+    id_venda   bigint,
+    quantidade integer,
+
+    PRIMARY KEY (id_bolo, id_venda),
+    FOREIGN KEY (id_bolo)
+        REFERENCES bolo (id),
+    FOREIGN KEY (id_venda)
+        REFERENCES venda (id_venda)
 );
 
 CREATE TRIGGER upperCaseOnInsertPerson
@@ -35,119 +146,6 @@ CREATE TRIGGER upperCaseOnInsertState
     FOR EACH ROW
     SET NEW.estado = UPPER(NEW.estado);
 
-CREATE TABLE cliente
-(
-    id        bigint primary key auto_increment unique not null,
-    id_pessoa bigint,
-    pontos    int check ( pontos >= 0 ),
-
-    FOREIGN KEY (id_pessoa)
-        REFERENCES pessoa (id)
-);
-
-CREATE TABLE funcionario
-(
-    id        bigint primary key auto_increment unique not null,
-    id_pessoa bigint,
-    salario   decimal(15, 2),
-    turno     enum ('M', 'T', 'N'),
-    cargo     varchar(50),
-
-    FOREIGN KEY (id_pessoa)
-        REFERENCES pessoa (id)
-);
-
-# TODO atualizar hora e data do BrModelo e Modelo Físico
-CREATE TABLE venda
-(
-    id_venda         bigint primary key auto_increment unique not null,
-    hora             datetime,
-    metodo_pagamento varchar(50)
-);
-
-CREATE TABLE pedido
-(
-    id_pedido      bigint unique not null,
-    id_venda       bigint,
-    id_cliente     bigint,
-    id_funcionario bigint,
-
-    PRIMARY KEY (id_pedido, id_venda, id_cliente, id_funcionario),
-    FOREIGN KEY (id_venda)
-        REFERENCES venda (id_venda),
-    FOREIGN KEY (id_cliente)
-        REFERENCES cliente (id),
-    FOREIGN KEY (id_funcionario)
-        REFERENCES funcionario (id)
-);
-
-CREATE TABLE itens
-(
-    id_pedido bigint,
-    item      varchar(255),
-
-    PRIMARY KEY (id_pedido, item),
-    FOREIGN KEY (id_pedido)
-        REFERENCES pedido (id_pedido)
-);
-
-CREATE TABLE bolo
-(
-    id        bigint primary key auto_increment unique not null,
-    nome      varchar(50),
-    descricao text,
-    tipo      varchar(30),
-    peso      integer check ( peso > 0 ),
-    sabor     varchar(40)
-);
-
-CREATE TABLE categoria
-(
-    id        bigint,
-    id_bolo   bigint,
-    descricao text,
-
-    PRIMARY KEY (id, id_bolo),
-    FOREIGN KEY (id_bolo)
-        REFERENCES bolo (id)
-);
-
-CREATE TABLE pedido_bolo
-(
-    id_pedido  bigint,
-    id_bolo    bigint,
-    quantidade integer check ( quantidade > 0 ),
-
-    PRIMARY KEY (id_pedido, id_bolo),
-    FOREIGN KEY (id_pedido)
-        REFERENCES pedido (id_pedido),
-    FOREIGN KEY (id_bolo)
-        REFERENCES bolo (id)
-);
-
-CREATE TABLE telefone
-(
-    id_pessoa bigint,
-    telefone  bigint(14),
-
-    PRIMARY KEY (id_pessoa, telefone),
-    FOREIGN KEY (id_pessoa)
-        REFERENCES pessoa (id)
-);
-
-
-CREATE TABLE dependente
-(
-    id              bigint unique not null,
-    id_funcionario  bigint,
-    nome            varchar(100),
-    data_nascimento date,
-
-    PRIMARY KEY (id, id_funcionario),
-    FOREIGN KEY (id_funcionario)
-        REFERENCES funcionario (id)
-);
-
--- DDL
+-- DML
 INSERT INTO pessoa(email, cpf, nome_completo, data_nascimento, rg, sexo, estado)
-VALUES ('leonardo.cadu2@gmail.com', '470.470.478-44', 'Leonardo Freitas', '1997-12-20', '50.555.555-5', 'm', 'sp')
+VALUES ('leonardo.cadu2@gmail.com', '470.470.478-44', 'Leonardo Freitas', '1997-12-20', '50.555.555-5', 'm', 'sp');
